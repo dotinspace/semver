ce8745a1534f272a4c5d0287fb0bd8935dc8d9f0
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-06-23 15:45:05 +0100

    add scm information

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject andreacrotti/semver "0.2.1"
   :description "Parser for semver specification"
   :url "http://github.com/andreacrotti/semver"
+  :scm {:name "git" :url "https://github.com/AndreaCrotti/semver"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]])
